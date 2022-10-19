aa4b6186f1d19c8a960d18500b5eb2734a6d3573
userbamb <110114819+userbamb@users.noreply.github.com>
2022-10-19 14:25:54 +0200

    Update effects.asm

--- data/moves/effects.asm
+++ data/moves/effects.asm
@@ -52,27 +52,6 @@ PoisonHit:
 	poisontarget
 	endmove
 
-Megaphone:
-    checkobedience
-	usedmovetext
-	doturn
-	critical
-	damagestats
-	damagecalc
-	stab
-	damagevariation
-	checkhit
-	moveanim
-	failuretext
-	applydamage
-	criticaltext
-	supereffectivetext
-	draintarget
-	checkfaint
-	buildopponentrage
-	kingsrock
-	endmove
-
 LeechHit:
 	checkobedience
 	usedmovetext
